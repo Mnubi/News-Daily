bd023f82ba7208d64b00f6a9225940fba4e831fd
mnubi <rodger.kamau@student.moringaschool.com>
2021-11-03 10:49:55 +0300

    deployment to heroku

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
 
     NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?apiKey={}'
     NEWS_API_ARTICLES_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
-
+    NEWS_API_KEY = '4c6387d3b12945de99414364a18d6452'
 
 class ProdConfig(Config):
     pass
